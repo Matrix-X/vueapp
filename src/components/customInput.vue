20489123ef7d3c14112602f97d82ecc1b347c3d4
hbj418 <hbj418@gmail.com>
2019-02-12 15:09:55 +0800

    finish custom input

--- src/components/customInput.vue
+++ src/components/customInput.vue
@@ -5,13 +5,39 @@
 <template>
 
   <div class="container">
-    <input type='text' v-model='coupon'>
+    <!-- <input type='text' v-model='coupon'> -->
+    <!-- <input type='text' :value='coupon' @input="coupon = $event.target.value"> -->
+    <coupon v-model='coupon'></coupon>
   </div>
 </template>
 
 <script>
 
 import Vue from 'vue'
+
+Vue.component('coupon', {
+  props:['code'],
+  data(){
+    return {
+      invalids: ['ALLFREE', 'SOMETHINGELSE']
+    }
+  },
+  template:`
+    <input type='text' :value='code' @input="updateCode($event.target.value)" ref="input">
+  `,
+  methods:{
+    updateCode(code){
+
+      if(this.invalids.includes(code)){
+        console.log("this coupon is no longer valid. Sorry!");
+        this.$refs.input.value = code = "";
+
+      }
+
+      this.$emit('input', code);
+    }
+  }
+});
 
 export default {
     name: 'CustomInput',
